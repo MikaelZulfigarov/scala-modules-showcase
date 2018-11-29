5cb33b6be3414ec67685aad835b8186413b341d8
Mikael Zulfigarov <mikaelzulfigarov@gmail.com>
2018-11-29 14:48:07 +0200

    Fix typo

--- src/main/scala/modules/upload/FileUploadModule.scala
+++ src/main/scala/modules/upload/FileUploadModule.scala
@@ -4,6 +4,6 @@ import com.google.inject.AbstractModule
 
 class FileUploadModule extends AbstractModule {
   override def configure(): Unit = {
-    bind(classOf[FileUploadService]).to(classOf[FileUploadService])
+    bind(classOf[FileUploadService]).to(classOf[FileUploadServiceImpl])
   }
 }
